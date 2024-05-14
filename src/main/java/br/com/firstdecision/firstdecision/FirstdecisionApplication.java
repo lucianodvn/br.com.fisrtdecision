0bca8dbc097a0658297ca35e75c4067bdf0d2907
Luciano Del Vecchio Neves <obrlucianodvn@hotmail.com>
2024-05-14 15:38:54 -0300

    commit inicial

---
+++ src/main/java/br/com/firstdecision/firstdecision/FirstdecisionApplication.java
@@ -0,0 +1,13 @@
+package br.com.firstdecision.firstdecision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstdecisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstdecisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/firstdecision/firstdecision/FirstdecisionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.firstdecision.firstdecision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstdecisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
